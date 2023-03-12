5b87986b8907d9bb1c1d19c8a5b8b6682e1de839
Nikappa57 <lorenz.gaudino@gmail.com>
2023-03-12 23:58:27 +0100

    improved search for find_b_elm

--- ft_push_to_b.c
+++ ft_push_to_b.c
@@ -6,7 +6,7 @@
 /*   By: lorenzogaudino <lorenzogaudino@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 13:11:22 by lorenzogaud       #+#    #+#             */
-/*   Updated: 2023/03/12 23:39:05 by lorenzogaud      ###   ########.fr       */
+/*   Updated: 2023/03/12 23:57:33 by lorenzogaud      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,27 +66,44 @@ static int	ft_move_elm_front_len(int stack_len, int elm_pos)
 		return (elm_pos - stack_len);
 }
 
+static int	ft_push_elm_to_b_len(int stack_a_len, int a_elm_pos,
+	int stack_b_len, int b_elm_pos)
+{
+	int	ra_n;
+	int	rb_n;
+
+	ra_n = ft_move_elm_front_len(stack_a_len, a_elm_pos);
+	rb_n = ft_move_elm_front_len(stack_b_len, b_elm_pos);
+	if (ra_n > 0 && rb_n > 0)
+		return (ft_max(ra_n, rb_n));
+	else if (ra_n < 0 && rb_n < 0)
+		return (ft_abs(ft_min(ra_n, rb_n)));
+	return (ft_abs(ra_n) + ft_abs(rb_n));
+}
+
 static int	ft_find_b_elm(
-	t_list *stack_a, t_list *stack_b, int a_eml_pos, int all_nbr_len)
+	t_list *stack_a, t_list *stack_b, int a_elm_pos, int stack_a_len)
 {
 	int	i;
 	int	a_val;
-	int	min_rb;
+	int	min_move;
 	int	min_b_pos;
 	int	stack_b_len;
 
 	stack_b_len = ft_lstsize(stack_b);
 	i = 0;
 	min_b_pos = 0;
-	min_rb = all_nbr_len;
-	a_val = ft_stack_elm_val(stack_a, a_eml_pos);
+	min_move = stack_a_len + stack_b_len;
+	a_val = ft_stack_elm_val(stack_a, a_elm_pos);
 	while (i < stack_b_len)
 	{
-		if (ft_is_in_chunk(all_nbr_len,
-				ft_get_chunk(a_val, all_nbr_len), stack_b->content)
-			&& min_rb > ft_abs(ft_move_elm_front_len(stack_b_len, i)))
+		if (ft_is_in_chunk(stack_a_len + stack_b_len, ft_get_chunk(
+					a_val, stack_a_len + stack_b_len), stack_b->content)
+			&& min_move > ft_push_elm_to_b_len(
+				stack_a_len, a_elm_pos, stack_b_len, i))
 		{
-			min_rb = ft_abs(ft_move_elm_front_len(stack_b_len, i));
+			min_move = ft_push_elm_to_b_len(
+					stack_a_len, a_elm_pos, stack_b_len, i);
 			min_b_pos = i;
 		}
 		stack_b = stack_b->next;
@@ -104,21 +121,11 @@ static void	ft_push_elm_to_b(
 	int	stack_b_len;
 
 	stack_b_len = ft_lstsize(*stack_b);
-	b_elm_pos = ft_find_b_elm(*stack_a, *stack_b, min_elm_pos,
-			stack_a_len + stack_b_len);
+	b_elm_pos = ft_find_b_elm(*stack_a, *stack_b, min_elm_pos, stack_a_len);
 	rb_n = ft_move_elm_front_len(stack_b_len, b_elm_pos);
 	ra_n = ft_move_elm_front_len(stack_a_len, min_elm_pos);
 	ft_r_best_combo(stack_a, stack_b, ra_n, rb_n);
 	ft_pb(stack_a, stack_b);
-}
-
-static int	ft_push_elm_to_b_len(int ra_n, int rb_n)
-{
-	if (ra_n > 0 && rb_n > 0)
-		return (ft_max(ra_n, rb_n));
-	else if (ra_n < 0 && rb_n < 0)
-		return (ft_abs(ft_min(ra_n, rb_n)));
-	return (ft_abs(ra_n) + ft_abs(rb_n));
 }
 
 void ft_stack_rmv_elm(t_list **stack, int elm_val)
@@ -172,10 +179,8 @@ static int	ft_min_elm(t_list *stack_a, t_list *stack_b,
 	while (nolis_list)
 	{
 		a_elm_pos = ft_stack_elm_pos(stack_a, nolis_list->content);
-		tmp_move = ft_push_elm_to_b_len(
-				ft_move_elm_front_len(stack_a_len, a_elm_pos),
-				ft_move_elm_front_len(stack_b_len, ft_find_b_elm(stack_a,
-						stack_b, a_elm_pos, stack_a_len + stack_b_len)));
+		tmp_move = ft_push_elm_to_b_len(stack_a_len, a_elm_pos, stack_b_len,
+				ft_find_b_elm(stack_a, stack_b, a_elm_pos, stack_a_len));
 		if (tmp_move < min_move && tmp_move != -1)
 		{
 			min_move = tmp_move;
@@ -243,6 +248,7 @@ void	ft_push_to_b(
 
 // vedere bene come divide i chunk
 // migliorare i nomi delle varie robe
-// migliorare conteggio mosse
+// migliorare conteggio mosse 
+// if (rb_n < 0) rb_n++;
 // 	per esempio quando sono 2 chunk perchè non li pusha insieme fance poi ra?
 // controllare i worst case (es ordinato ma invertito)
